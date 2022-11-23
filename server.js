cfaedc38daccee327ecb859bd6d1368316f3a5c7
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-11-23 21:54:08 +0100

    final

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
 require("dotenv").config(); // this line is mandatory when you want to read variables from the '.env' file
 
-const name = process.env.MY_NAME;
-const city = process.env.MY_CITY;
-const language = process.env.MY_LANGUAGE;
+const name = process.env.NAME;
+const city = process.env.CITY;
+const language = process.env.LANGUAGE;
 const message = `I am ${name}, wilder in ${city}, and I love ${language}`;
 
 console.log(message);
